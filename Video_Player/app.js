3793c137cf01df800f44fb53a53a9995e3730cc0
AlimaKarimova <way.s@inbox.ru>
2022-07-14 18:41:50 +0400

    readme file was added

--- Video_Player/app.js
+++ Video_Player/app.js
@@ -6,49 +6,50 @@ document.querySelector("#speed-down").onclick = slow;
 document.querySelector("#speed-normal").onclick = normal;
 document.querySelector("#volume").oninput = volume;
 
-let video;
-let display;
-let progress;
-video = document.querySelector("#video-player");
-progress = document.querySelector("#progress");
+let controlsContainer = document.querySelector("#controls-container");
+let video = document.querySelector("#video-player");
+let progress = document.querySelector("#progress");
+let volumePanel=document.querySelector('#volume-panel')
+let playSpeed = 1;
+
 video.ontimeupdate = progressUpdate;
 progress.onclick = videoRewind;
+
 function play() {
-  video.play();
+   video.play();
 }
 function pause() {
-  video.pause();
+   video.pause();
 }
 function stop() {
-  video.pause();
-  video.currentTime = 0;
+   video.pause();
+   video.currentTime = 0;
 }
 function fast() {
-  video.play();
-  video.playbackRate = 2;
+   playSpeed += 0.25;
+   video.playbackRate = playSpeed;
 }
 function slow() {
-  video.play();
-  video.playbackRate = 0.5;
+   playSpeed -= 0.25;
+   video.playbackRate = playSpeed;
 }
 function normal() {
-  video.play();
-  video.playbackRate = 1;
+   video.playbackRate = 1;
 }
 function volume() {
-  let v = this.value;
-  video.volume = v / 100; //in percent
+   let v = this.value;
+   video.volume = v / 100; //in percent
 }
 function progressUpdate() {
-  let c = video.currentTime;
-  let d = video.duration;
-  progress.value = (100 * c) / d;
+   let c = video.currentTime;
+   let d = video.duration;
+   progress.value = (100 * c) / d;
 }
 function videoRewind(event) {
-  let w = this.offsetWidth;
-  let o = event.offsetX;
-  this.value = (100 * o) / w;
-  video.pause();
-  video.currentTime = video.duration * (o / w);
-  video.play();
+   let w = this.offsetWidth;
+   let o = event.offsetX;
+   this.value = (100 * o) / w;
+   video.pause();
+   video.currentTime = video.duration * (o / w);
+   video.play();
 }
---
+++ Video_Player/readme.md
@@ -0,0 +1,10 @@
+### Methods and properties
+
+-  video.play()
+-  video.pause()
+-  video.playbackRate
+-  video.currentTime
+-  video.volume
+-  video.duration
+-  event.offsetX
+-  offsetWidth
